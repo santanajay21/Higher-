4e0f51a83eea2826498e54dfc2592b6badb2a2d1
Jeremy Santana <72940290+santanajay21@users.noreply.github.com>
2025-03-04 21:21:08 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+"""import modules"""
+import random
+from game_data import data
+from art import logo
+from art import vs
+
+
+user_not_wrong = True
+"""create a while loop to continue playing while the user is not wrong"""
+while user_not_wrong:
+    print(logo)
+
+
+
